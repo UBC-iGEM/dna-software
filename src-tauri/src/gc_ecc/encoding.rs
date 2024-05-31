167661b7a3707c3aed33493171b62b0b9741045b
Riya <riya03.wall@gmail.com>
2024-06-01 00:33:52 +0200

    encoding step1 function

--- src-tauri/src/gc_ecc/encoding.rs
+++ src-tauri/src/gc_ecc/encoding.rs
@@ -1,14 +1,30 @@
 // 1. binary to q-ary
-fn bin_to_qblocks(bin_seq: &str) {
+fn bin_to_qblocks(bin_seq: &str) -> Result<Vec<String>, String> {
+    let k = bin_seq.len();
+    let q = (k as f64).log2() as usize;
+
+    let block_size = k / q;
+    let mut qary_seq = Vec::new();
+
+    for i in 0..q {
+        let start = i * block_size;
+        let end = start + block_size;
+        let block = &bin_seq[start..end];
+        qary_seq.push(block.to_string());
+    }
+
+    Ok(qary_seq)
+}
+
+fn map_block_to_finite(blocks: Vec<String>) {
     
 }
 
-fn map_block_to_finite(blocks: Vec) {
-
-}
-
 // 2. convert to systematic MDS code
-fn convert_to_mds(blocks: Vec) {
+fn convert_to_mds(blocks: Vec, num_del:i8) {
+    let deletions = num_del;
+    let c = deletions + 1;
+
 
 }
 
@@ -17,7 +33,15 @@ fn mds_to_bin(blocks: Vec) {
 
 }
 
-// 4. repeat parity bits
-fn repeat_parity(blocks: Vec) {
+fn main() {
+    let bin_seq = "1100110011001100"; // Example binary sequence
 
+    match bin_to_qblocks(bin_seq) {
+        Ok(qary_seq) => {
+            println!("Q-ary sequence: {:?}", qary_seq);
+        }
+        Err(e) => {
+            println!("Error: could not convery binary to q-blocks", e);
+        }
+    }
 }
