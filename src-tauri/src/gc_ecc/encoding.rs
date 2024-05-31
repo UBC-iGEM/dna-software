8ee2ac72b6f3c8427f789773bbea0a59dc27e0e9
Riya <riya03.wall@gmail.com>
2024-06-01 00:00:51 +0200

    encoding stubs

---
+++ src-tauri/src/gc_ecc/encoding.rs
@@ -0,0 +1,23 @@
+// 1. binary to q-ary
+fn bin_to_qblocks(bin_seq: &str) {
+    
+}
+
+fn map_block_to_finite(blocks: Vec) {
+
+}
+
+// 2. convert to systematic MDS code
+fn convert_to_mds(blocks: Vec) {
+
+}
+
+// 3. q-ary to binary
+fn mds_to_bin(blocks: Vec) {
+
+}
+
+// 4. repeat parity bits
+fn repeat_parity(blocks: Vec) {
+
+}
