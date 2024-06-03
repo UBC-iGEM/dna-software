d0cc4f985e61bc60f49709ef5b4a7f8eed2911dd
Sebastian Hyland <st.hyland05@gmail.com>
2024-06-02 23:56:16 -0700

    Fixed code errors and tested; chaosDNA is functional

--- src-tauri/src/chaosDNA.rs
+++ src-tauri/src/chaosDNA.rs
@@ -1,23 +1,24 @@
-use rand::Rng;
-use std::{io, iter};
-use diff::Diff;
+use rand::{Rng, distributions::{Distribution, Standard}};
+// use diff::Diff;
 
 
 pub trait ChaosDNA {
-    fn modify_base(&self, input: &Vec<Base>, prob: f64) -> Vec<Base>; 
-    fn insert_base(&self, input: &Vec<Base>, prob: f64) -> Vec<Base>;
-    fn apply_chaos(&self, input: &Vec<Base>, ins_prob: f64, mod_prob: f64) -> Vec<Base>;
-    fn diff_chaos(&self, input: &Vec<Base>, ins_prob: f64, mod_prob: f64) -> Vec<diff::Result<Base>>;
+    fn modify_base(&self, initial_seq: &Vec<Base>, prob: f64) -> Vec<Base>; 
+    fn insert_base(&self, initial_seq: &Vec<Base>, prob: f64) -> Vec<Base>;
+    fn full_chaos(&self, initial_seq: &Vec<Base>, mod_prob: f64, ins_prob: f64) -> Vec<Base>;
+//    fn diff_chaos(&self, input: &Vec<Base>, ins_prob: f64, mod_prob: f64) -> Vec<diff::Result<Base>>;
 }
+
 
 pub struct ErrorDNA {
 }
+
 
 impl ChaosDNA for ErrorDNA {
     // Takes a base vector and probability as input, outputs a base vector with modified bases
     fn modify_base(&self, initial_seq: &Vec<Base>, prob: f64) -> Vec<Base> {
 	let mut rng = rand::thread_rng();
-	input
+	initial_seq
 	    .iter()
 	    .map(|base| {
 		// RNG boolean; if true, keeps running until a different base from the original is selected
@@ -45,20 +46,32 @@ impl ChaosDNA for ErrorDNA {
 	    inserted_seq.push(rng.gen::<Base>());
 	}
 	// Insert random base(s) after current
-	for base in input {
+	for base in initial_seq {
 	    inserted_seq.push(*base);
 	    while rng.gen_bool(prob) {
 		inserted_seq.push(rng.gen::<Base>());
 	    }
 	}
+	inserted_seq
     }
 
     fn full_chaos(&self, initial_seq: &Vec<Base>, mod_prob: f64, ins_prob: f64) -> Vec<Base> {
 	let mod_seq = self.modify_base(initial_seq, mod_prob);
-	self.insert_base(mod_seq, ins_prob)
+	self.insert_base(&mod_seq, ins_prob)
     }
 
-    fn diff_chaos(&self, initial_seq: &Vec<Base>, mod_prob: f64, ins_prob: f64) -> Vec<diff::Result<Base>> {
-	todo()!
-    }
+//    fn diff_chaos(&self, initial_seq: &Vec<Base>, mod_prob: f64, ins_prob: f64) -> Vec<diff::Result<Base>> {
+//	todo()!
+//    }
 }
+
+
+// Here is some example code that I used to test:
+
+
+// fn main() {
+//     let chaos = ErrorDNA{};
+//     let initial_sequence = vec![Base::G, Base::T, Base::A, Base::C, Base::C, Base::G, Base::A, Base::T, Base::T, Base::G];
+//     let modified_sequence = chaos.full_chaos(&initial_sequence, 0.2, 0.2);
+//     println!("{:?}", modified_sequence);
+// }
