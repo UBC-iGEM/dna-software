2510acfd712a6b9f1d7c4cc821ee2d635659cd5d
Sebastian Hyland <st.hyland05@gmail.com>
2024-07-02 23:47:03 -0700

    Added sanity checks and documentation

--- src-tauri/src/chaosdna.rs
+++ src-tauri/src/chaosdna.rs
@@ -13,7 +13,8 @@ pub struct Chaos {
 
 
 impl RandError for Chaos {
-    // Takes a base vector and probability as input, outputs a base vector with modified bases
+
+    // Takes a base as input, outputs a different base.
     fn modify_base(&self, input_base: &Base) -> Base {
 	let mut rng = rand::thread_rng();
 	loop {
@@ -24,20 +25,25 @@ impl RandError for Chaos {
 	}
     }
 
-
+    // Takes a vector of bases, an edit probability, and a deletion prob as input.
+    // Outputs a vector of bases that may be modified.
     fn full_chaos(&self, initial_seq: &Vec<Base>, edit_prob: f64, del_prob: f64) -> Vec<Base> {
 	let mut rng = rand::thread_rng();
 	initial_seq
 	    .iter()
 	    .filter_map(|base| {
+		// If the edit probability boolean passes...
 		if rng.gen_bool(edit_prob) {
+		    // Delete if deletion prob boolean passes.
 		    if rng.gen_bool(del_prob) {
 			None
 		    }
+		    // Modify if deletion prob boolean fails.
 		    else {
 			Some(self.modify_base(base))
 		    }
 		}
+		// If the edit prob boolean fails, return the original base.
 		else {
 		    Some(*base)
 		}
@@ -52,6 +58,8 @@ mod tests {
     use super::*;
     use crate::primer::Base;
 
+    // Generate a random vector of bases and execute full_chaos on it.
+    // Output is printed.
     #[test]
     fn test_mutate() {
         let chaos = Chaos {};
@@ -72,5 +80,57 @@ mod tests {
 	    print!("{}", base);
 	}
 	println!();
+    }
+
+    // Use full_chaos on a random vector of bases with edit_prob set to 0.
+    // Output should be identical to input.
+    #[test]
+    fn chaos_sanity_no_edit() {
+	let chaos = Chaos{};
+	let mut rng = rand::thread_rng();
+
+        let initial_sequence: Vec<Base> = (0..10)
+	    .map(|_| rng.gen::<Base>())
+	    .collect();
+
+	let modified_sequence = chaos.full_chaos(&initial_sequence, 0.0, 0.5);
+
+	assert_eq!(initial_sequence, modified_sequence);
+    }
+
+    // Use full_chaos with edit_prob and del_prob set to 0.
+    // All bases should be deleted.
+    #[test]
+    fn chaos_sanity_del_all() {
+	let chaos = Chaos{};
+	let mut rng = rand::thread_rng();
+
+	
+        let initial_sequence: Vec<Base> = (0..10)
+	    .map(|_| rng.gen::<Base>())
+	    .collect();
+
+	let modified_sequence = chaos.full_chaos(&initial_sequence, 1.0, 1.0);
+
+	assert_eq!(modified_sequence.len(), 0);
+    }
+
+    // Use full_chaos with del_prob set to 0.
+    // Output length should be same as input.
+    #[test]
+    fn chaos_sanity_only_mut() {
+
+	let chaos = Chaos{};
+	let mut rng = rand::thread_rng();
+
+	
+        let initial_sequence: Vec<Base> = (0..10)
+	    .map(|_| rng.gen::<Base>())
+	    .collect();
+
+	let modified_sequence = chaos.full_chaos(&initial_sequence, 0.5, 0.0);
+
+	assert_eq!(modified_sequence.len(), initial_sequence.len());
+	assert_eq!(modified_sequence.len(), 10);
     }
 }
