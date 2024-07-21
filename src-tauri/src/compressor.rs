9d5df900db017d9268501c454ea0718dc3b1b274
Sebastian Hyland <st.hyland05@gmail.com>
2024-07-21 13:37:04 -0700

    feat(compression): added decompression and lz4 - may be broken

--- src-tauri/src/compressor.rs
+++ src-tauri/src/compressor.rs
@@ -5,8 +5,12 @@ use std::{
 };
 
 pub trait Compressor {
-    fn compress(&self, filepath: PathBuf) -> PathBuf;
-    fn compress_llm(&self, filepath: PathBuf) -> PathBuf;
+    fn compress(&self, inpath: PathBuf) -> PathBuf;
+    fn compress_llm(&self, inpath: PathBuf) -> PathBuf;
+    fn compress_lz4(&self, inpath: PathBuf) -> PathBuf;
+    fn decompress(&self, inpath: PathBuf) -> PathBuf;
+    fn decompress_llm(&self, inpath: PathBuf) -> PathBuf;
+    fn decompress_lz4(&self, inpath: PathBuf) -> PathBuf;
 }
 
 pub struct VoidCompressor {}
@@ -16,7 +20,7 @@ impl Compressor for VoidCompressor {
         // Access the extension and compare it to string literals
         match inpath.extension().and_then(|os_str| os_str.to_str()) {
             Some("txt") | Some("org") | Some("md") => self.compress_llm(inpath),
-            _ => inpath,
+            _ => self.compress_lz4(inpath),
         }
     }
 
@@ -26,13 +30,12 @@ impl Compressor for VoidCompressor {
         let filename = inpath.file_stem().unwrap();
         let outpath = Path::new("/path/to/data/dir")
             .join(filename)
-            .with_extension("bin"); // Use with_extension for consistency
+            .with_extension("bin");
         let mut process = Command::new("/path/to/ts_zip")
-            // Assuming ts_zip takes arguments this way, otherwise adjust accordingly
             .arg("-m")
             .arg("path/to/model")
             .arg("c")
-            .arg(inpath.as_path().to_str().unwrap()) // Convert PathBuf to &str for display
+            .arg(inpath.as_path().to_str().unwrap()) 
             .arg(outpath.as_path().to_str().unwrap())
             .stdout(Stdio::piped())
             .spawn()
@@ -67,5 +70,41 @@ impl Compressor for VoidCompressor {
         }
 
         outpath
+    }
+
+    fn compress_lz4(&self, inpath: PathBuf) -> PathBuf {
+        compress(&inpath, &inpath.with_extension("lz4")).unwrap();
+        inpath.with_extension("lz4")
+    }
+
+    fn decompress(&self, inpath: PathBuf) -> PathBuf {
+        // Access the extension and compare it to string literals
+        match inpath.extension().and_then(|os_str| os_str.to_str()) {
+            Some("bin") => self.decompress_llm(inpath),
+            _ => self.decompress_lz4(inpath),
+        }
+    }
+
+    // Use ts_zip and pipe output to mut progress
+    // Output code from https://stackoverflow.com/questions/31576555/unable-to-pipe-to-or-from-spawned-child-process-more-than-once/31577297#31577297
+    fn decompress_llm(&self, inpath: PathBuf) -> PathBuf {
+        let filename = inpath.file_stem().unwrap();
+        let outpath = Path::new("/path/to/data/dir")
+            .join(filename)
+            .with_extension("txt");
+        let mut process = Command::new("/path/to/ts_zip")
+            // Assuming ts_zip takes arguments this way, otherwise adjust accordingly
+            .arg("-m")
+            .arg("path/to/model")
+            .arg("d")
+            .arg(inpath.as_path().to_str().unwrap()) // Convert PathBuf to &str for display
+            .arg(outpath.as_path().to_str().unwrap())
+            .output()
+            .expect("decompression process");
+        outpath
+    }
+
+    fn decompress_lz4(&self, inpath: PathBuf) -> PathBuf {
+        decompress(&inpath, &inpath.with_extension("")).unwrap();
     }
 }
