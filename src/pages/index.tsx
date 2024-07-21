3f3aa981cdd4f7d8eaa621188955da55d9cd544d
lucyhao <hao.lucyy@gmail.com>
2024-07-20 23:32:19 -0700

    chore: add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import logo from '../logo.png';
+import { Center } from "../components/Center";
+
 const Home = () => {
   return (
     <div>
       <h1>nuCloud</h1>
+      <Center>
+      <img src={logo} alt="Logo" />
+      </Center>
     </div>
   );
 };
