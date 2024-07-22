2b8b0ca06fe0161faec53bb8158742e6ee4fbbd8
Sebastian Hyland <st.hyland05@gmail.com>
2024-07-22 14:27:35 -0700

    fix: added feature definition to main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 #![feature(array_windows)]
 #![feature(iter_map_windows)]
 #![feature(iter_array_chunks)]
+#![feature(diagnostic_namespace)]
 
 use primer::{Base, MeltingTemperature, Primer, PrimerInfo};
 #[cfg(test)]
