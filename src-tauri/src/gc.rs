121374bde589d4f4cbbd87c6a6ca0ef11817720d
lucyhao <hao.lucyy@gmail.com>
2024-07-21 17:22:51 -0700

    feature: binary to q-ary and back works + tested

--- src-tauri/src/gc.rs
+++ src-tauri/src/gc.rs
@@ -1,43 +1,73 @@
-use bitvec::{order::Msb0, vec::BitVec};
-
+use bitvec::{order::Msb0, slice::BitSlice, vec::BitVec};
+use g2p;
 struct GF {}
 impl GF {}
 
-// TODO: consider making a trait if we do other error correction strategies that just operate on bits
+// TODO: consider making a trait if we do other error correction strategies that just operate and return bits
 
 struct GcEncoder {}
 impl GcEncoder {
-    fn bin_to_qblocks(bin_seq: &str) -> Result<Vec<String>, String> {
+    fn bin_to_qblocks(bin_seq: &mut BitVec<u8, Msb0>) -> Vec<&BitSlice<u8, Msb0>> {
         let k = bin_seq.len();
-        let q = (k as f64).log2() as usize;
+        let k_next_power_of_two = k.next_power_of_two();
+        let block_length = (k_next_power_of_two as f64).log2() as usize;
+        let num_symbols = k_next_power_of_two.div_ceil(block_length);
+        let leading_zeros_needed = (num_symbols * block_length) - k;
 
-        let block_size = k / q;
-        let mut qary_seq = Vec::new();
-
-        for i in 0..q {
-            let start = i * block_size;
-            let end = start + block_size;
-            let block = &bin_seq[start..end];
-            qary_seq.push(block.to_string());
+        for _ in 0..leading_zeros_needed {
+            bin_seq.insert(0, false);
         }
 
-        Ok(qary_seq)
+        assert_eq!(bin_seq.len(), num_symbols * block_length);
+
+        let q_ary: Vec<&BitSlice<u8, Msb0>> = bin_seq.chunks(block_length).collect();
+
+        q_ary
     }
 
-    fn map_block_to_finite(blocks: Vec<String>) {
+    fn map_block_to_finite(blocks: Vec<&BitSlice<u8, Msb0>>) {
         todo!()
     }
 
     // 2. convert to systematic MDS code
-    // TODO: block type might not be right, just want to get this to compile
-    fn convert_to_mds(blocks: &BitVec<u8, Msb0>, num_del: i8) {
+    fn convert_to_mds(blocks: Vec<&BitSlice<u8, Msb0>>, num_del: usize) {
         let deletions = num_del;
         let c = deletions + 1;
         todo!()
     }
 
     // 3. q-ary to binary
-    fn mds_to_bin(blocks: &BitVec<u8, Msb0>) {
-        todo!()
+    fn mds_to_bin(blocks: Vec<&BitSlice<u8, Msb0>>) -> BitVec<u8, Msb0> {
+        blocks.into_iter().flatten().collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bitvec::{order::Msb0, vec::BitVec};
+
+    use crate::gc::GcEncoder;
+
+    #[quickcheck]
+    fn test_qblocking(bytes: Vec<u8>) -> bool {
+        if bytes.len() != 0 {
+            let mut bits = BitVec::from_vec(bytes.clone());
+            let bits_to_compare: BitVec<u8, Msb0> = BitVec::from_vec(bytes);
+            let qblocks = GcEncoder::bin_to_qblocks(&mut bits);
+            let bits = GcEncoder::mds_to_bin(qblocks);
+            bits_to_compare.count_ones() == bits.count_ones()
+        } else {
+            true
+        }
+    }
+
+    #[test]
+    fn test_basic() {
+        let bytes = vec![8];
+        let mut bits = BitVec::from_vec(bytes.clone());
+        let bits_to_compare: BitVec<u8, Msb0> = BitVec::from_vec(bytes);
+        let qblocks = GcEncoder::bin_to_qblocks(&mut bits);
+        let returned_bits = GcEncoder::mds_to_bin(qblocks);
+        assert_eq!(bits_to_compare.count_ones(), returned_bits.count_ones());
     }
 }
