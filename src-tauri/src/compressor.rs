700cd84585bf3a2d11321f3192470702214ce70c
Sebastian Hyland <st.hyland05@gmail.com>
2024-08-01 12:26:22 -0700

    fix(readability): shifted functions to improve code legibility

--- src-tauri/src/compressor.rs
+++ src-tauri/src/compressor.rs
@@ -29,6 +29,50 @@ impl Compressor for VoidCompressor {
         self.compress_lz4(inpath)
     }
 
+    fn compress_lz4(&self, inpath: PathBuf) -> Result<PathBuf> {
+        let mut input_file = File::open(inpath.as_path())?;
+        let filename = inpath.file_stem().unwrap();
+        let outpath = Path::new("/path/to/data/dir")
+            .join(filename)
+            .with_extension("lz4");
+        let output_file = File::create(outpath.as_path())?;
+
+        println!("Compressing: {} -> {}", inpath.display(), outpath.display());
+
+        let mut encoder = EncoderBuilder::new().level(12).build(output_file)?;
+        io::copy(&mut input_file, &mut encoder)?;
+        let (mut writer, result) = encoder.finish();
+        result?;
+        writer.flush()?;
+
+        Ok(outpath)
+    }
+
+    fn decompress(&self, inpath: PathBuf, outpath: PathBuf) -> Result<()> {
+        //  Currently aliases to decompress_lz4. May use pattern matching in future
+        //
+        //  match inpath.extension().and_then(|os_str| os_str.to_str()) {
+        //      Some("bin") => self.decompress_llm(inpath, outpath),
+        //      _ => self.decompress_lz4(inpath, outpath),
+        //  }
+        self.decompress_lz4(inpath, outpath)
+    }
+
+    fn decompress_lz4(&self, inpath: PathBuf, outpath: PathBuf) -> Result<()> {
+        println!(
+            "Decompressing: {} -> {}",
+            inpath.display(),
+            outpath.display()
+        );
+
+        let input_file = File::open(inpath.as_path())?;
+        let mut decoder = Decoder::new(input_file)?;
+        let mut output_file = File::create(outpath.as_path())?;
+        io::copy(&mut decoder, &mut output_file)?;
+
+        Ok(())
+    }
+
     // Use ts_zip and pipe output to mut progress
     // Progress display code from https://stackoverflow.com/questions/31576555/unable-to-pipe-to-or-from-spawned-child-process-more-than-once/31577297#31577297, untested
     fn compress_llm(&self, inpath: PathBuf) -> Result<PathBuf> {
@@ -82,35 +126,6 @@ impl Compressor for VoidCompressor {
         Ok(outpath)
     }
 
-    fn compress_lz4(&self, inpath: PathBuf) -> Result<PathBuf> {
-        let mut input_file = File::open(inpath.as_path())?;
-        let filename = inpath.file_stem().unwrap();
-        let outpath = Path::new("/path/to/data/dir")
-            .join(filename)
-            .with_extension("lz4");
-        let output_file = File::create(outpath.as_path())?;
-
-        println!("Compressing: {} -> {}", inpath.display(), outpath.display());
-
-        let mut encoder = EncoderBuilder::new().level(12).build(output_file)?;
-        io::copy(&mut input_file, &mut encoder)?;
-        let (mut writer, result) = encoder.finish();
-        result?;
-        writer.flush()?;
-
-        Ok(outpath)
-    }
-
-    fn decompress(&self, inpath: PathBuf, outpath: PathBuf) -> Result<()> {
-        //  Currently aliases to decompress_lz4. May use pattern matching in future
-        //
-        //  match inpath.extension().and_then(|os_str| os_str.to_str()) {
-        //      Some("bin") => self.decompress_llm(inpath, outpath),
-        //      _ => self.decompress_lz4(inpath, outpath),
-        //  }
-        self.decompress_lz4(inpath, outpath)
-    }
-
     fn decompress_llm(&self, inpath: PathBuf, outpath: PathBuf) -> Result<()> {
         let mut process = Command::new("/path/to/ts_zip")
             .arg("-m")
@@ -120,21 +135,6 @@ impl Compressor for VoidCompressor {
             .arg(outpath.as_path().to_str().unwrap())
             .output()
             .expect("decompression process");
-
-        Ok(())
-    }
-
-    fn decompress_lz4(&self, inpath: PathBuf, outpath: PathBuf) -> Result<()> {
-        println!(
-            "Decompressing: {} -> {}",
-            inpath.display(),
-            outpath.display()
-        );
-
-        let input_file = File::open(inpath.as_path())?;
-        let mut decoder = Decoder::new(input_file)?;
-        let mut output_file = File::create(outpath.as_path())?;
-        io::copy(&mut decoder, &mut output_file)?;
 
         Ok(())
     }
